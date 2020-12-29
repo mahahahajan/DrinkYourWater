d3a2cb8b1f5499556e87983f192029dd7744abb6
mahahahajan <pulkit8.mahajan@gmail.com>
2020-12-28 21:01:30 -0800

    Initial Commit

---
+++ DrinkWater/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DrinkWater
+//
+//  Created by Pulkit Mahajan on 12/28/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
