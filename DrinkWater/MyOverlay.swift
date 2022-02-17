e46a95b5b2dc4b2e61bf3395735d280b6fd26db7
mahahahajan <pulkit8.mahajan@gmail.com>
2022-02-16 21:32:24 -0600

    i dont think i did the GUI commit right

---
+++ DrinkWater/MyOverlay.swift
@@ -0,0 +1,15 @@
+//
+//  MyOverlay.swift
+//  DrinkWater
+//
+//  Created by Pulkit Mahajan on 2/15/22.
+//
+
+import Foundation
+import Cocoa
+
+class MyOverlay: NSPanel {
+    override var canBecomeKey: Bool {
+        return true
+    }
+}
